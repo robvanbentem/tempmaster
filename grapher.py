00881c7dbf332973fec59e2a5e4921c9eb12c803
Rob van Bentem <robvanbentem@gmail.com>
2015-08-09 15:12:41 +0200

    optimized settings

--- grapher.py
+++ grapher.py
@@ -13,18 +13,18 @@ def makegraphs():
 
     # define the graphs we want
     graphs = [
-        ['1h', '60', 'last 1 hour', []],
-        ['8h', '480', 'last 8 hours', []],
-        ['1d', '1440', 'last 1 day', []],
-        ['7d', '10080', 'last 7 days', []]
+        ['1h', '120', 'last 1 hour', []],
+        ['8h', '600', 'last 8 hours', []],
+        ['1d', '1800', 'last 1 day', []],
+        ['7d', '3600', 'last 7 days', []]
     ]
 
     for opts in graphs:
-        gmake(rrdpath, tspan=opts[0], step=opts[1], title=opts[2], extra=opts[3], smooth=False)
+        gmake(rrdpath, tspan=opts[0], step=opts[1], title=opts[2], extra=opts[3])
 
 
 # make graph
-def gmake(rrd, tspan, step='300', title='', smooth=False, extra=[]):
+def gmake(rrd, tspan, step='300', title='', extra=[]):
     rrdtool.graph(imgpath + 'temp' + tspan +'.png',
             '--imgformat', 'PNG',
             '--width',  '720',
@@ -34,7 +34,6 @@ def gmake(rrd, tspan, step='300', title='', smooth=False, extra=[]):
             '--title', 'Temperature ' + title,
             '-A',
             '-S', step,
-            ('-E' if smooth else []),
             '--alt-y-grid',
             'DEF:avg=' + rrd + ':a:AVERAGE',
             'DEF:min=' + rrd + ':a:MIN',
